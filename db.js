5a7a4b22a7972d12eb79606a92e14e6836e17f8a
santhiyasara22 <santhusara2207@gmail.com>
2025-06-17 18:29:17 +0530

    Changes

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',          
-  database: 'postax_db'    
+  database: 'form_postax'    
 });
 
 db.connect((err) => {
