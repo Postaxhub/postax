2a6872a4d55f7d31b59b596c98acff2aa4c175e1
santhiyasara22 <santhusara2207@gmail.com>
2025-06-17 10:41:06 +0530

    ss

--- contact-api.php
+++ contact-api.php
@@ -1,15 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json');
 
-
+// Collect form data
 $name = $_POST['name'] ?? '';
 $email = $_POST['email'] ?? '';
 $subject = $_POST['subject'] ?? '';
 $message = $_POST['message'] ?? '';
 
-
+// Basic validation
 if ($name && $email && $subject && $message) {
-    $to = "santhiya8334@gmail.com";
+    $to = "santhiya8334@gmail.com"; // CHANGE to your actual email
     $email_subject = "New Contact Form Message - $subject";
     $email_body = "Name: $name\nEmail: $email\nMessage:\n$message";
     $headers = "From: $email";
