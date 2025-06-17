a1bc185102932dcf3079e2f0a205e6cd6107b153
santhiyasara22 <santhusara2207@gmail.com>
2025-06-17 10:33:48 +0530

    changes

---
+++ contact-api.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+
+
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$subject = $_POST['subject'] ?? '';
+$message = $_POST['message'] ?? '';
+
+
+if ($name && $email && $subject && $message) {
+    $to = "santhiya8334@gmail.com";
+    $email_subject = "New Contact Form Message - $subject";
+    $email_body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo json_encode(['status' => 'success', 'message' => 'Message sent successfully!']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Mail sending failed.']);
+    }
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Please fill in all fields.']);
+}
+?>
